4eec0fecb820fec799400558b981a91a684d59be
Fatih <fatihce57@gmail.com>
2021-07-16 01:22:19 +0200

    react installed and component created

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+
+
+function Card() {
+  return (
+    <div className="Card">
+     <h1>a</h1>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+     <h1>b</h1>
+    </div>
+  );
+}
+
+export default Navbar;
