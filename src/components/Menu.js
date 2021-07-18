6e7c39d085df594dc9331f74861d1c60ecaf0771
Fatih <fatihce57@gmail.com>
2021-07-18 04:19:21 +0200

    navbar modified

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,18 +2,18 @@ import React from 'react';
 import "./Menu.css";
 import CardData from "./Card.json";
 
-function Menu () {
+function Menu() {
     return (
         <div className="Menu">
-            {CardData.map(item=>{
-                return(            
-            <div className="card">
-                <h2 className="card-title">{item.menuName}</h2>
-                <h6 className="price">{item.productPrice}</h6>
-                <img src={item.menuIMG} alt="menu_image" />
-                <p className="card-desc">{item.menuRecipe}</p>
-            </div>
-            )
+            {CardData.map(item => {
+                return (
+                    <div key={item.productID} className="card">
+                        <h2 className="card-title">{item.menuName}</h2>
+                        <h6 className="price">{item.productPrice}</h6>
+                        <img src={item.menuIMG} alt="menu_image" />
+                        <p className="card-desc">{item.menuRecipe}</p>
+                    </div>
+                )
             })}
         </div>
     )
