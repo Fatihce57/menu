2734ff7d68bea29432b62533a4aadda89a1dfb5f
Fatih <fatihce57@gmail.com>
2021-07-23 16:01:32 +0200

    header picture added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,20 @@ import {useState} from "react";
 function Navbar() {
 
 
-const handleBreakfast=()=>{
-  
+// const HandleBreakfast=()=>{
+//   const[data, setData]=useState(0);
 
-}
+//  const 
+// }
 
 
   return (
     <div className="Navbar">
+      <img src="../assets/header.jpg" alt="restaurant" width="100%" height="150px" />
      <h1 className="nav-header">FATIH'S MENU</h1>
       <nav className="nav-list">
         <li>All</li>
-        <li onClick={handleBreakfast}>Breakfast</li>
+        {/* <li onClick={HandleBreakfast}>Breakfast</li> */}
         <li>Lunch</li>
         <li>Drinks</li>
         <li>Specials</li>
